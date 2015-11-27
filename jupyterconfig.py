d67488560ca29cdae27585a2473da83009a2b13b
Patrick Buech <patrick.buech@siemens.com>
2015-11-27 08:53:29 +0100

    Check database url

--- jupyterconfig.py
+++ jupyterconfig.py
@@ -10,19 +10,21 @@ c.NotebookApp.password = IPython.lib.passwd(
     os.getenv('JUPYTER_NOTEBOOK_PASSWORD', default=str(uuid.uuid4())))
 
 ### PostresContentsManager ###
-# Tell IPython to use PostgresContentsManager for all storage.
-c.NotebookApp.contents_manager_class = pgcontents.PostgresContentsManager
-
-# Set the url for the database used to store files.  See
-# http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
-# for more info on db url formatting.
-c.PostgresContentsManager.db_url = os.getenv('DATABASE_URL')
-
-# PGContents associates each running notebook server with a user, allowing
-# multiple users to connect to the same database without trampling each other's
-# notebooks. By default, we use the result of result of getpass.getuser(), but
-# a username can be specified manually like so:
-c.PostgresContentsManager.user_id = 'heroku'
-
-# Set a maximum file size, if desired.
-#c.PostgresContentsManager.max_file_size_bytes = 1000000 # 1MB File cap
+database_url = os.getenv('DATABASE_URL', None)
+if database_url:
+    # Tell IPython to use PostgresContentsManager for all storage.
+    c.NotebookApp.contents_manager_class = pgcontents.PostgresContentsManager
+    
+    # Set the url for the database used to store files.  See
+    # http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html#postgresql
+    # for more info on db url formatting.
+    c.PostgresContentsManager.db_url = database_url
+    
+    # PGContents associates each running notebook server with a user, allowing
+    # multiple users to connect to the same database without trampling each other's
+    # notebooks. By default, we use the result of result of getpass.getuser(), but
+    # a username can be specified manually like so:
+    c.PostgresContentsManager.user_id = 'heroku'
+    
+    # Set a maximum file size, if desired.
+    #c.PostgresContentsManager.max_file_size_bytes = 1000000 # 1MB File cap
