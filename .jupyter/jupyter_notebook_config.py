3bc258d9ef798678e598f43d20c5accaf019549f
Patrick Büch <git@paco.pl31.de>
2017-01-10 07:40:41 +0100

    Add empty token for password free access

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -8,9 +8,13 @@ try:
     c = get_config()
 
     ### Password protection ###
+    # http://jupyter-notebook.readthedocs.io/en/latest/security.html
     if os.environ.get('JUPYTER_NOTEBOOK_PASSWORD_DISABLED') != 'DangerZone!':
         passwd = os.environ['JUPYTER_NOTEBOOK_PASSWORD']
         c.NotebookApp.password = IPython.lib.passwd(passwd)
+    else:
+        c.NotebookApp.token = ''
+        c.NotebookApp.password = ''
 
     ### PostresContentsManager ###
     database_url = os.getenv('DATABASE_URL', None)
